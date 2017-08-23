09406f63e76f15286fc2145e7f78903a754f25b2
Hovik Melikyan <hovik.melikyan@gmail.com>
2017-08-23 16:28:32 +0100

    Use Camera1 API for everything

--- library/src/main/java/com/google/android/cameraview/CameraView.java
+++ library/src/main/java/com/google/android/cameraview/CameraView.java
@@ -97,13 +97,23 @@ public class CameraView extends FrameLayout {
         // Internal setup
         final PreviewImpl preview = createPreviewImpl(context);
         mCallbacks = new CallbackBridge();
-        if (Build.VERSION.SDK_INT < 21) {
-            mImpl = new Camera1(mCallbacks, preview);
-        } else if (Build.VERSION.SDK_INT < 23) {
-            mImpl = new Camera2(mCallbacks, preview, context);
-        } else {
-            mImpl = new Camera2Api23(mCallbacks, preview, context);
-        }
+
+
+        // Camera2 is problematic even on some phones that support API 21+, therefore we currently
+        // use Camera1 for everything, because it just seems to work on all hardware. Particularly
+        // problematic was the front camera on Sony Xperia compact series phones. -- H.M.
+        //
+        // if (Build.VERSION.SDK_INT < 21) {
+        //     mImpl = new Camera1(mCallbacks, preview);
+        // } else if (Build.VERSION.SDK_INT < 23) {
+        //     mImpl = new Camera2(mCallbacks, preview, context);
+        // } else {
+        //     mImpl = new Camera2Api23(mCallbacks, preview, context);
+        // }
+
+        mImpl = new Camera1(mCallbacks, preview);
+
+
         // Attributes
         TypedArray a = context.obtainStyledAttributes(attrs, R.styleable.CameraView, defStyleAttr,
                 R.style.Widget_CameraView);
